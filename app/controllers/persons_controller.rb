2ecc9220cc97a6b0cf0fb5e442ffbf7a6caeffc2
al <alfaalbert@gmail.com>
2016-10-07 11:34:44 +0300

    heroku

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,13 @@
 class PersonsController < ApplicationController
   def profile
+
+  end
+
+  def aboutUs
+
+  end
+
+  def ourWork
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'persons/profile'
-
+  get 'persons/aboutUs'
+  get 'persons/ourWork'
   root 'persons#profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
