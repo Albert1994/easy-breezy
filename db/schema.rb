0f5a0d3062be8ff5b4932dcc8685759354958d94
Maxim <odmaxim07@gmail.com>
2016-10-10 23:13:48 +0300

    chop

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20161008154018) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
